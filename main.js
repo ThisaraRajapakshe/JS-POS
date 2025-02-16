28e2ae54620de804417fbb24ceb901b369c469fd
ThisaraRajapakshe <thisararajapakshe2020@gmail.com>
2025-02-16 15:23:53 +0530

    Added ID and Card view
    Added IDs to product list and created card view product list

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 let products = [
-    {id: 1, name: 'Smartphone', price: 299},
-    {id: 2, name: 'Laptop', price: 899},
-    {name: 'Tablet', price: 199},
-    {name: 'Smartwatch', price: 149},
-    {name: 'Wireless Earbuds', price: 79},
-    {name: 'Bluetooth Speaker', price: 59},
-    {name: 'Gaming Headset', price: 129},
-    {name: 'Walkie-Talkie', price: 49},
-    {name: 'Wireless Router', price: 99},
-    {name: 'Landline Phone', price: 39},
-    {name: 'Webcam', price: 89},
-    {name: 'Microphone', price: 119},
-    {name: 'Portable WiFi Hotspot', price: 129},
-    {name: 'VR Headset', price: 299},
-    {name: 'Satellite Phone', price: 599},
-    {name: 'Two-Way Radio', price: 79},
-    {name: 'USB-C Hub', price: 29},
-    {name: 'Ethernet Cable', price: 19},
-    {name: 'Power Bank', price: 49},
-    {name: 'Wireless Keyboard', price: 79},
-    {name: 'Wireless Mouse', price: 39}
+    {id: 1, name: 'Smartphone', price: 97175},
+    {id: 2, name: 'Laptop', price: 292175},
+    {id: 3, name: 'Tablet', price: 64675},
+    {id: 4, name: 'Smartwatch', price: 48425},
+    {id: 5, name: 'Wireless Earbuds', price: 25675},
+    {id: 6, name: 'Bluetooth Speaker', price: 19175},
+    {id: 7, name: 'Gaming Headset', price: 41925},
+    {id: 8, name: 'Walkie-Talkie', price: 15875},
+    {id: 9, name: 'Wireless Router', price: 32175},
+    {id: 10, name: 'Landline Phone', price: 12675},
+    {id: 11, name: 'Webcam', price: 28925},
+    {id: 12, name: 'Microphone', price: 38675},
+    {id: 13, name: 'Portable WiFi Hotspot', price: 41925},
+    {id: 14, name: 'VR Headset', price: 97175},
+    {id: 15, name: 'Satellite Phone', price: 194675},
+    {id: 16, name: 'Two-Way Radio', price: 25675},
+    {id: 17, name: 'USB-C Hub', price: 9435},
+    {id: 18, name: 'Ethernet Cable', price: 6175},
+    {id: 19, name: 'Power Bank', price: 15875},
+    {id: 20, name: 'Wireless Keyboard', price: 25675},
+    {id: 21, name: 'Wireless Mouse', price: 12675}
 ];
+
 
 products.forEach(element => {
     const div = document.createElement('div')
     //div.innerHTML = element.name + ' : Rs: ' + element.price;
-    div.style = "border: solid"
+    div.style = "width: 250px; height: 150px; border: solid; float: left; margin: 10px"
 
     const p = document.createElement('p');
-    p.innerText =  element.name + ' : Rs: ' + element.price;
+    p.innerText =  element.name +' : Rs: ' + element.price;
 
     div.append(p);
 
